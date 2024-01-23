70bc800ddfd49c2e6a1972b645258329816dcead
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-22 20:24:29 -0500

    Update spot_instance.tf

--- spot_instance.tf
+++ spot_instance.tf
@@ -9,17 +9,5 @@ resource "aws_spot_instance_request" "cheap_worker" {
   }
 }
 resource "aws_spot_instance_request" "public" {
-  ami                  = "ami-0b3e57ee3b63dd76b" # amazon linux 2 ami
-  spot_price           = "0.03"
-  instance_type        = "t3a.nano"
-  subnet_id            = module.vpc.public_subnets[0]
-  iam_instance_profile = aws_iam_instance_profile.main.id
-}
-
-resource "aws_spot_instance_request" "spot1" {
-  ami                    = "ami-02701bcdc5509e57b"
-  spot_price             = "0.016"
-  instance_type          = "t2.micro"
-  spot_type              = "one-time"
-  wait_for_fulfillment   = "true"
+  ami                  = "ami-0b3e57ee3b63dd76b" # amazon linux 
 }
