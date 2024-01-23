52b0a594862eaf0069ddda8639bd25536b19186f
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-22 20:35:15 -0500

    Update spot_instance.tf

--- spot_instance.tf
+++ spot_instance.tf
@@ -1,15 +1,15 @@
 # Request a spot instance at $0.03
-resource "aws_spot_instance_request" "cheap_worker" {
+resource "aws_spot_instance_request" "spot1" {
   ami           = "ami-1234"
   spot_price    = "0.03"
   instance_type = "c4.xlarge"
 
   tags = {
-    Name = "CheapWorker"
+    Name = "Spotinstance"
   }
 }
 
-resource "aws_spot_instance_request" "spot1" {
+resource "aws_spot_instance_request" "spot2" {
   ami                    = "ami-02701bcdc5509e57b"
   spot_price             = "0.016"
   instance_type          = "t2.micro"
@@ -17,6 +17,16 @@ resource "aws_spot_instance_request" "spot1" {
   wait_for_fulfillment   = "true"
 
   tags = {
-    Name = "SpotInstanceOne"
+    Name = "SpotInstanceTwo"
+  }
+}
+resource "aws_spot_instance_request" "spot3" {
+  ami           = "ami-0c94855ba95c574c8" # Amazon Linux 2 AMI in us-east-1
+  instance_type = "t3.micro"
+  spot_price    = "0.02"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "SpotInstanceThree"
   }
 }
