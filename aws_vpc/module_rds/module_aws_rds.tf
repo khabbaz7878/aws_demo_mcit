97b201f1786033a671be0e320e4a0575d252981a
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-24 18:48:51 -0500

    Update module_aws_rds.tf

--- aws_vpc/module_rds/module_aws_rds.tf
+++ aws_vpc/module_rds/module_aws_rds.tf
@@ -1,27 +1,32 @@
-module "rdsdb" {
- source = "terraform-aws-modules/rds/aws"
- version = "~> 2.0"
 
- identifier = "mydb"
+module "rds" {
+  source = "terraform-aws-modules/rds/aws"
+  version = "~> 2.0"
 
- engine            = "mysql"
- engine_version    = "5.7"
- instance_class    = "db.t2.large"
- allocated_storage = 5
+  identifier = "mydb"
 
- name     = "mydb"
- username = "foo"
- password = "foobarbaz"
- port     = "3306"
+  engine            = "mysql"
+  engine_version    = "5.7"
+  instance_class    = "db.t2.large"
+  allocated_storage = 5
 
- vpc_security_group_ids = ["sg-12345678"]
+  name     = "mydb"
+  username = "foo"
+  password = "foobarbaz"
+  port     = "3306"
 
- maintenance_window = "Mon:00:00-Mon:03:00"
- backup_window      = "03:00-06:00"
+  vpc_security_group_ids = ["sg-12345678"]
 
- # Enhanced Monitoring - see example for details on how to create the role
- # by yourself, you need to assign the required permissions to the role.
- monitoring_interval    = "30"
- monitoring_role_name   = "MyRDSMonitoringRole"
- create_monitoring_role = true
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, you need to assign the required permissions to the role.
+  monitoring_interval    = "30"
+  monitoring_role_name   = "MyRDSMonitoringRole"
+  create_monitoring_role = true
+}
+
+output "rds_endpoint" {
+  value = module.rds.db_instance_endpoint
 }
