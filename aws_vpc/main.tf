aefa87320c8fd2e72db399cbd150521a606954bb
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-24 19:16:29 -0500

    Update main.tf

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -1,50 +1,6 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "example-vpc"
-  }
-}
-
-resource "aws_subnet" "example" {
-  count = 2
-
-  cidr_block = "10.0.${count.index + 1}.0/24"
-  vpc_id     = aws_vpc.example.id
-
-  tags = {
-    Name = "example-subnet-${count.index + 1}"
-  }
-}
-
-resource "aws_internet_gateway" "example" {
-  vpc_id = aws_vpc.example.id
-
-  tags = {
-    Name = "example-igw"
-  }
-}
-
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
-  }
-
-  tags = {
-    Name = "example-route-table"
-  }
-}
-
-resource "aws_route_table_association" "example" {
-  count = 2
-
-  subnet_id      = aws_subnet.example[count.index].id
-  route_table_id = aws_route_table.example.id
+module "aws_vpc" {
+  source          = "./modules/vpc-module"
+  vpc_cidr_block  = "10.0.0.0/16"
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 }
