a370b7f5741c8e91179854630641c6ba9377d83c
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-24 19:25:20 -0500

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public" {
   count = length(var.public_subnets)
 
-  vpc_id                  = module.aws_vpc.vpc_id
+  vpc_id                  = module.vpc.vpc_id
   cidr_block              = var.public_subnets[count.index]
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
@@ -14,7 +14,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   count = length(var.private_subnets)
 
-  vpc_id                  = module.aws_vpc.vpc_id
+  vpc_id                  = module.vpc.vpc_id
   cidr_block              = var.private_subnets[count.index]
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
 
