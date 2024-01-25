6159ab8fc55ae7be3dfeb53945eeeb042231075f
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-24 19:10:21 -0500

    Update module_aws_vpc.tf

--- module_aws_vpc.tf
+++ module_aws_vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
+  version = "4.0.0"
 
   name            = "${var.name}-${local.name}"
   cidr            = var.cidr
